1c936d7f951e092288b5b20078c943e08d12439d
Daniel Cripps <dancripps.dc@gmail.com>
2017-09-14 13:19:23 +0100

    Refactor keyboard input

--- wm.c
+++ wm.c
@@ -1,17 +1,36 @@
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+
 struct termios orig_termios;
 
+/*** terminal ***/
+
+void die(const char *s) {
+	perror(s);
+	exit(1);
+}
+
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		die("tcsetattr");
 }
 
 void enableRawMode() {
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+		die("tcgetattr");
+
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
@@ -22,21 +41,38 @@ void enableRawMode() {
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		die("tcsetattr");
 }
+
+char editorReadKey() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+/*** input ***/
+
+void editorProcessKeypress() {
+	char c = editorReadKey();
+
+	switch(c) {
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
+/*** init ***/
 
 int main() {
 	enableRawMode();
-
+	
 	while (1) {
-		char c = '\0';
-		read(STDIN_FILENO, &c, 1);
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == 'q') break;
+		editorProcessKeypress();
 	}
 
 	return 0;
