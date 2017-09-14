af00a19d38c938188baa780582d34ea12a36752d
Daniel Cripps <dancrippsdc@gmail.com>
2017-09-14 14:52:42 +0100

    Clear the screen on exit

--- wm.c
+++ wm.c
@@ -18,6 +18,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -54,6 +57,13 @@ char editorReadKey() {
 	return c;
 }
 
+/*** output ***/
+
+void editorRefreshScreen() {
+	write(STDOUT_FILENO, "\x1b[2j", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -61,6 +71,8 @@ void editorProcessKeypress() {
 
 	switch(c) {
 		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
 	}
@@ -72,6 +84,7 @@ int main() {
 	enableRawMode();
 	
 	while (1) {
+		editorRefreshScreen();
 		editorProcessKeypress();
 	}
 
